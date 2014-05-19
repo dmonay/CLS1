5ba09cbfe2744e5b6f665154aab4ebcf65ffd252
dmonay <pavluk73@gmail.com>
2014-05-19 11:13:55 -0400

    initial

---
+++ test node app/lib/rapgenius-app.js
@@ -0,0 +1,13 @@
+/*
+ * rapGenius_app
+ * index.html
+ *
+ * Copyright (c) 2014 Dmitry Pavluk
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+exports.awesome = function() {
+  return 'awesome';
+};
